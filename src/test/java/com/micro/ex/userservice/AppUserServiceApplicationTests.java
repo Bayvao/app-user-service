8034017937d20baa87d4561302b82d9648bcb4da
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2021-09-11 22:13:23 +0530

    first commit

---
+++ src/test/java/com/micro/ex/userservice/AppUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.ex.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
