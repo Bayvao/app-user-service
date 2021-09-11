48fbca5194c2c320cca3ef022d5ef6077dfb6bad
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2021-09-11 22:43:04 +0530

    added eureka discovery client for user microservice

--- src/main/java/com/micro/ex/userservice/AppUserServiceApplication.java
+++ src/main/java/com/micro/ex/userservice/AppUserServiceApplication.java
@@ -2,8 +2,10 @@ package com.micro.ex.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AppUserServiceApplication {
 
 	public static void main(String[] args) {
