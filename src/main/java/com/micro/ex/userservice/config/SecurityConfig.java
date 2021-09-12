a0fb3beb37388e070f07943103f41bc63b5e19cc
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2021-09-12 19:16:45 +0530

    using bcrypt password encoder to encode password

--- src/main/java/com/micro/ex/userservice/config/SecurityConfig.java
+++ src/main/java/com/micro/ex/userservice/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.micro.ex.userservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -12,9 +15,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/**").hasIpAddress("127.0.0.1");
+		http.authorizeRequests().antMatchers("/v1/**").hasIpAddress("127.0.0.1");
+		http.authorizeRequests().antMatchers("/h2-console").permitAll();
 		http.headers().frameOptions().disable();
 			
+	}
+	
+	@Bean
+	public PasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	
--- src/main/java/com/micro/ex/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/micro/ex/userservice/service/impl/UserServiceImpl.java
@@ -1,28 +1,35 @@
 package com.micro.ex.userservice.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.micro.ex.userservice.entity.User;
 import com.micro.ex.userservice.model.UserRequest;
 import com.micro.ex.userservice.repository.UserRepository;
 import com.micro.ex.userservice.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	PasswordEncoder bCryptPasswordEncoder;
 
 	@Override
-	public void createUser(UserRequest userDetails) {
+	public UserRequest createUser(UserRequest userDetails) {
 		User user = new User();
 		user.setUserId(userDetails.getEmail());
 		user.setEmailId(userDetails.getEmail());
 		user.setFirstName(userDetails.getFirstName());
 		user.setLastName(userDetails.getLastName());
-		user.setPassword(null);
+		user.setPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		
 		userRepository.save(user);
 		
+		return userDetails;
 	}
 
 }
